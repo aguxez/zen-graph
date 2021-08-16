3e6cebf9b842df9e15dc92daf86aa58509965d62
Miguel Diaz <diazmiiguel@gmail.com>
2021-08-16 00:43:09 -0500

    init :chains:

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,56 +1,26 @@
 import { BigInt } from "@graphprotocol/graph-ts"
 import {
-  Core,
-  TokenAddedToTrade,
+  TokenAddedToTrade as TokenAddedToTradeEvent,
   TokenRemovedFromTrade,
   TradeExtended,
   TradeFinalized,
-  TradeStarted,
+  TradeStarted as TradeStartedEvent,
   UserTradeStateChange
 } from "../generated/Core/Core"
-import { ExampleEntity } from "../generated/schema"
+import {
+  TokenAddedToTrade,
+  TradeStarted
+} from "../generated/schema";
 
-export function handleTokenAddedToTrade(event: TokenAddedToTrade): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
+export function handleTokenAddedToTrade(event: TokenAddedToTradeEvent): void {
+  let entity = new TokenAddedToTrade(event.transaction.hash.toHex())
+  entity.blockNumber = event.block.number.toI32();
+  entity.tradeId = event.params._tradeId.toString();
+  entity.owner = event.params._owner;
+  entity.tokenId = event.params._tokenId.toI32();
+  entity.cell = event.params._cell.toI32();
 
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (entity == null) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
-
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
-  }
-
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
-
-  // Entity fields can be set based on event parameters
-  entity._tradeId = event.params._tradeId
-  entity._owner = event.params._owner
-
-  // Entities can be written to the store with `.save()`
   entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.getTrade(...)
-  // - contract.onERC721Received(...)
 }
 
 export function handleTokenRemovedFromTrade(
@@ -61,6 +31,16 @@ export function handleTradeExtended(event: TradeExtended): void {}
 
 export function handleTradeFinalized(event: TradeFinalized): void {}
 
-export function handleTradeStarted(event: TradeStarted): void {}
+export function handleTradeStarted(event: TradeStartedEvent): void {
+  let entity = new TradeStarted(event.transaction.hash.toHex());
+  entity.blockNumber = event.block.number.toI32();
+  entity.tradeId = event.params._tradeId.toString();
+  entity.starter = event.params._starter;
+  entity.receiver = event.params._receiver;
+  entity.starterContract = event.params._starterContract;
+  entity.receiverContract = event.params._receiverContract;
+
+  entity.save();
+}
 
 export function handleUserTradeStateChange(event: UserTradeStateChange): void {}
